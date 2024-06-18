72f11ae689f70fa94c8d6df53f0625ad93ff35c1
Maria Petersen <maria.petersen@ealico.com>
2024-06-18 13:58:05 +0200

    ADD get offers usecase, controller and get offers method

--- web-service/src/core/offre/ports/IOfferRepository.ts
+++ web-service/src/core/offre/ports/IOfferRepository.ts
@@ -1,5 +1,8 @@
 import { Offre } from "../domain/Offre";
+import { IOfferFilter } from "../filter/IOfferFilter";
 
 export interface IOfferRepository {
     getFirsts(limit: number): Promise<Offre[]>;
+    getOffers(limit: number, filters: IOfferFilter): Promise<Offre[]>;
+    createOffersQueryWithFilters(limit: number, filters: IOfferFilter): { query: string, options: string[]}
 }
