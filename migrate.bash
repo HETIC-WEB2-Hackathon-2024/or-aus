48964e379ec24a15fbc9c56b4476c34a8f340f2b
Alessandro Garau <alessandrogarau.pro@gmail.com>
2024-06-18 16:09:28 +0200

    migrate readme et bash

---
+++ migrate.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Migrating database"
+
+for file in ./migration/*; do
+    echo "Running migration: $file"
+    open "$file"
+    
+    if [ ! -e "$file" ]; then
+        continue
+    fi
+
+    docker container run --env PGPASSWORD=aus2025 \
+        -v "$(pwd)"/migration:/sql/migration \
+        --rm \
+        --network=web \
+        --platform linux/amd64 \
+        postgis/postgis:16-3.4-alpine \
+        psql -h postgres-aus -p 5432 -U aus-user -d aus -f /sql/migration/"$(basename "$file")"
+done
