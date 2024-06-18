d92093d507bf5034ae6c81397269523194b0eb0d
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-06-18 16:51:39 +0200

    REMOVE unwanted char

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,18 +6,17 @@ import { Authenticated } from "./auth/Authenticated.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-
     <Auth0Provider
-        domain="or-aus.eu.auth0.com"
-        clientId="yq89EpaAOUiwyXNJKq7VgTeIg7iGJBw0"
-        authorizationParams={{
-          audience: "api.or.aus.floless.fr",
-          redirect_uri: window.location.origin
-        }}
-      >
+      domain="or-aus.eu.auth0.com"
+      clientId="yq89EpaAOUiwyXNJKq7VgTeIg7iGJBw0"
+      authorizationParams={{
+        audience: "api.or.aus.floless.fr",
+        redirect_uri: window.location.origin,
+      }}
+    >
       <Authenticated>
         <App />
       </Authenticated>
-    </Auth0Provider>,
+    </Auth0Provider>
   </React.StrictMode>
 );
