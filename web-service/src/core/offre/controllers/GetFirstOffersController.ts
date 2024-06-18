2be5fbf7e0e0c09c2a0d2190db31b9011085260e
Louisan Tchitoula <t.louisan@outlook.fr>
2024-06-18 10:43:44 +0200

    Fix controller binding

--- web-service/src/core/offre/controllers/GetFirstOffersController.ts
+++ web-service/src/core/offre/controllers/GetFirstOffersController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { GetFirstOffersUseCase } from "../ports/GetFirstOffersUseCase";
 import { InvalidRequestError } from "express-oauth2-jwt-bearer";
+import { IUseCase } from "../../../shared/IUseCase";
 
 export class GetFirstOffersController {
-    public constructor(private readonly _useCase: GetFirstOffersUseCase) {}
+    public constructor(private readonly _useCase: GetFirstOffersUseCase) {
+        this.handle = this.handle.bind(this);
+    }
 
     public async handle(req: Request, res: Response) {
         try {
-            const limit = parseInt(req.body.limit);
+            const limit = parseInt(req.query.limit as string);
             if (!limit) throw new InvalidRequestError("limit must be set");
 
             const result = await this._useCase.execute({ limit });
@@ -15,7 +18,10 @@ export class GetFirstOffersController {
         } catch (error) {
             if (error instanceof InvalidRequestError)
                 res.status(parseInt(error.code)).send({ error: error.message, reason: error });
-            else res.status(500).send({ error: "Internal Server Error", reason: error });
+            else if (error instanceof Error) {
+                console.error(error);
+                res.status(500).send({ error: error.message, reason: error });
+            }
         }
     }
 }
