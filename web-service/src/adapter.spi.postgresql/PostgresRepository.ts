5fa62049e25bea5062a9172e576222b8ace91df3
Louisan Tchitoula <t.louisan@outlook.fr>
2024-06-18 15:58:55 +0200

    Fix Filter help class method

--- web-service/src/adapter.spi.postgresql/PostgresRepository.ts
+++ web-service/src/adapter.spi.postgresql/PostgresRepository.ts
@@ -5,76 +5,12 @@ import { IUserRepository } from "../core/user/ports/IUserRepository";
 import { Offre } from "../core/offre/domain/Offre";
 import { TUserId } from "../core/user/domain/Utilisateur";
 import { IOfferFilter } from "../core/offre/filter/IOfferFilter";
-
-interface IQuery {
-    query: string;
-    options: string[];
-}
+import { FilterHelper } from "../core/offre/shared/Filter-helper";
 
 export class PostgresRepository implements IOfferRepository, IUserRepository {
     public constructor(private readonly _pool: Pool) {}
-
-    createOffersQueryWithFilters(limit: number, filters: IOfferFilter): IQuery {
-        let optionNumber = 2;
-        const options: string[] = [limit.toString()];
-        let query = `SELECT * FROM offre JOIN date_debut_stage ON date_debut_stage.offre_id = offre.id`;
-        if (Object.keys(filters).length > 0) {
-            query += ` WHERE`;
-            Object.keys(filters).forEach((filter) => {
-                switch (filter) {
-                    case "metier_id":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` metier_id = $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.metier_id) options.push(filters.metier_id);
-                        break;
-                    case "secteur_id":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` secteur_id = $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.secteur_id) options.push(filters.secteur_id);
-                        break;
-                    case "type_contrat":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` type_contrat = $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.type_contrat) options.push(filters.type_contrat);
-                        break;
-                    case "commune_id":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` commune_id = $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.commune_id) options.push(filters.commune_id);
-                        break;
-                    case "entreprise":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` entreprise = $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.entreprise) options.push(filters.entreprise);
-                        break;
-                    case "search":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` titre_emploi LIKE $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.search) options.push(`%${filters.search}%`);
-                        break;
-                    case "period_start":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` debut_stage >= $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.period_start) options.push(filters.period_start.toString());
-                        break;
-                    case "period_end":
-                        if (optionNumber > 2) query += ` AND`;
-                        query += ` debut_stage <= $${optionNumber}`;
-                        optionNumber++;
-                        if (filters.period_end) options.push(filters.period_end.toString());
-                        break;
-                }
-            });
-        }
-        query += ` LIMIT $1`;
-        return { query, options };
+    getUserSecteurOffersCount(user_id: TUserId): Promise<number> {
+        throw new Error("Method not implemented.");
     }
 
     async getRegisteredOffers(user_id: TUserId): Promise<Offre[]> {
@@ -96,7 +32,7 @@ export class PostgresRepository implements IOfferRepository, IUserRepository {
     async getOffers(limit: number, filters: IOfferFilter) {
         const client = await this._pool.connect();
         try {
-            const queryWithFilters = this.createOffersQueryWithFilters(limit, filters);
+            const queryWithFilters = FilterHelper.createOffersQueryWithFilters(limit, filters);
             const results = await client.query<Offre>(queryWithFilters.query, queryWithFilters.options);
             return results.rows;
         } finally {
