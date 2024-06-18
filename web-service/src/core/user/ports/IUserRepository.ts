e2f99355a8af3562e1731191dcca4e85138e066c
dreadrea8 <inc.katsuro@outlook.fr>
2024-06-18 16:59:07 +0200

    Add repo getUserCommunecount

--- web-service/src/core/user/ports/IUserRepository.ts
+++ web-service/src/core/user/ports/IUserRepository.ts
@@ -2,6 +2,7 @@ import { Offre } from "../../offre/domain/Offre";
 import { TUserId } from "../domain/Utilisateur";
 
 export interface IUserRepository {
-    getRegisteredOffers(user_id: TUserId): Promise<Offre[]>;
-    getUserApplicationsCount(user_id: TUserId): Promise<number>;
+  getRegisteredOffers(user_id: TUserId): Promise<Offre[]>;
+  getUserApplicationsCount(user_id: TUserId): Promise<number>;
+  getUserCommuneCount(user_id: TUserId): Promise<number>;
 }
