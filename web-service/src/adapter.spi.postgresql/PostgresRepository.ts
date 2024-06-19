aed3d127e41f25d2e96b99b3891c64343c981da2
dreadrea8 <inc.katsuro@outlook.fr>
2024-06-19 14:08:05 +0200

    getCandidatCandidaturesCount

--- web-service/src/adapter.spi.postgresql/PostgresRepository.ts
+++ web-service/src/adapter.spi.postgresql/PostgresRepository.ts
@@ -1,28 +1,49 @@
-import { Pool } from "pg";
+import { Client, Pool } from "pg";
 
 import { IOfferRepository } from "../core/offre/ports/IOfferRepository";
 import {
-    ICandidatCommuneOffersStatsResponse,
-    ICandidatRepository,
-    ICandidatSecteurOffersStatsResponse,
+  ICandidatCommuneOffersStatsResponse,
+  ICandidatRepository,
+  ICandidatSecteurOffersStatsResponse,
 } from "../core/candidat/ports/ICandidatRepository";
 import { Offre } from "../core/offre/domain/Offre";
 import { IOfferFilter } from "../core/offre/filter/IOfferFilter";
 import { FilterHelper } from "../core/offre/shared/Filter-helper";
 import { TCandidatId } from "../core/candidat/domain/Candidat";
 
-export class PostgresRepository implements IOfferRepository, ICandidatRepository {
-    public constructor(private readonly _pool: Pool) {}
-    getCandidatCandidaturesCount(user_id: TCandidatId): Promise<number> {
-        throw new Error("Method not implemented.");
-    }
+export class PostgresRepository
+  implements IOfferRepository, ICandidatRepository
+{
+  public constructor(private readonly _pool: Pool) {}
 
-    async getCandidatSecteurOffersStats(
-        input: TCandidatId
-    ): Promise<Omit<ICandidatSecteurOffersStatsResponse, "comparison_percentage">> {
-        const client = await this._pool.connect();
-        try {
-            const query = `
+  async getCandidatCandidaturesCount(input: TCandidatId): Promise<number> {
+    const client = await this._pool.connect();
+    try {
+      const query = `SELECT 
+                        COUNT(*) AS nombre
+                        FROM 
+                        public.offre AS o
+                        JOIN 
+                        public.candidat_communes AS c ON c.commune_id = o.commune_id
+                        WHERE c.candidat_id = $1`;
+      const {
+        rows: [result],
+      } = await client.query<{ nombre: number }>(query, [input.id]);
+
+      return result.nombre;
+    } finally {
+      client.release();
+    }
+  }
+
+  async getCandidatSecteurOffersStats(
+    input: TCandidatId
+  ): Promise<
+    Omit<ICandidatSecteurOffersStatsResponse, "comparison_percentage">
+  > {
+    const client = await this._pool.connect();
+    try {
+      const query = `
                 SELECT
                     COUNT(CASE WHEN date_trunc('month', o.date) = date_trunc('month', CURRENT_DATE) THEN 1 END) AS current_month,
                     COUNT(CASE WHEN date_trunc('month', o.date) = date_trunc('month', CURRENT_DATE - INTERVAL '1 month') THEN 1 END) AS previous_month,
@@ -33,32 +54,34 @@ export class PostgresRepository implements IOfferRepository, ICandidatRepository
                 WHERE cs.candidat_id = $1
                 GROUP BY s.secteur`;
 
-            const {
-                rows: [result],
-            } = await client.query<Omit<ICandidatSecteurOffersStatsResponse, "comparison_percentage">>(query, [
-                input.id,
-            ]);
+      const {
+        rows: [result],
+      } = await client.query<
+        Omit<ICandidatSecteurOffersStatsResponse, "comparison_percentage">
+      >(query, [input.id]);
 
-            return {
-                current_month: +result.current_month,
-                previous_month: +result.previous_month,
-                secteur: result.secteur,
-            };
-        } finally {
-            client.release();
-        }
+      return {
+        current_month: +result.current_month,
+        previous_month: +result.previous_month,
+        secteur: result.secteur,
+      };
+    } finally {
+      client.release();
     }
+  }
 
-    async getRegisteredOffers(input: TCandidatId): Promise<Offre[]> {
-        throw new Error("Method not implemented.");
-    }
+  async getRegisteredOffers(input: TCandidatId): Promise<Offre[]> {
+    throw new Error("Method not implemented.");
+  }
 
-    async getCandidatCommuneOffersStats(
-        input: TCandidatId
-    ): Promise<Omit<ICandidatCommuneOffersStatsResponse, "comparison_percentage">> {
-        const client = await this._pool.connect();
-        try {
-            const query = `
+  async getCandidatCommuneOffersStats(
+    input: TCandidatId
+  ): Promise<
+    Omit<ICandidatCommuneOffersStatsResponse, "comparison_percentage">
+  > {
+    const client = await this._pool.connect();
+    try {
+      const query = `
                 SELECT 
                     COUNT(CASE WHEN date_trunc('month', o.date) = date_trunc('month', CURRENT_DATE) THEN 1 END) AS current_month,
                     COUNT(CASE WHEN date_trunc('month', o.date) = date_trunc('month', CURRENT_DATE - INTERVAL '1 month') THEN 1 END) AS previous_month,
@@ -72,31 +95,37 @@ export class PostgresRepository implements IOfferRepository, ICandidatRepository
                 GROUP BY co.nom_commune, co.code_postal;
                 `;
 
-            const {
-                rows: [result],
-            } = await client.query<Omit<ICandidatCommuneOffersStatsResponse, "comparison_percentage">>(query, [
-                input.id,
-            ]);
+      const {
+        rows: [result],
+      } = await client.query<
+        Omit<ICandidatCommuneOffersStatsResponse, "comparison_percentage">
+      >(query, [input.id]);
 
-            return {
-                current_month: +result.current_month,
-                previous_month: +result.previous_month,
-                commune: result.commune,
-                code_postal: result.code_postal,
-            };
-        } finally {
-            client.release();
-        }
+      return {
+        current_month: +result.current_month,
+        previous_month: +result.previous_month,
+        commune: result.commune,
+        code_postal: result.code_postal,
+      };
+    } finally {
+      client.release();
     }
+  }
 
-    async getOffers(limit: number, filters: IOfferFilter) {
-        const client = await this._pool.connect();
-        try {
-            const queryWithFilters = FilterHelper.createOffersQueryWithFilters(limit, filters);
-            const results = await client.query<Offre>(queryWithFilters.query, queryWithFilters.options);
-            return results.rows;
-        } finally {
-            client.release();
-        }
+  async getOffers(limit: number, filters: IOfferFilter) {
+    const client = await this._pool.connect();
+    try {
+      const queryWithFilters = FilterHelper.createOffersQueryWithFilters(
+        limit,
+        filters
+      );
+      const results = await client.query<Offre>(
+        queryWithFilters.query,
+        queryWithFilters.options
+      );
+      return results.rows;
+    } finally {
+      client.release();
     }
+  }
 }
--- web-service/src/core/offre/controllers/GetOffersController.ts
+++ web-service/src/core/offre/controllers/GetOffersController.ts
@@ -4,27 +4,28 @@ import { GetOffersUseCase } from "../ports/GetOffersUseCase";
 import { IOfferFilter } from "../filter/IOfferFilter";
 
 export class GetOffersController {
-    public constructor(private readonly _useCase: GetOffersUseCase) {
-        this.handle = this.handle.bind(this);
-    }
+  public constructor(private readonly _useCase: GetOffersUseCase) {
+    this.handle = this.handle.bind(this);
+  }
 
-    public async handle(req: Request, res: Response) {
-        try {
-            let limit = parseInt(req.query.limit as string);
-            if (!limit) {
-                limit = 50
-            }
-            const queryString = req.query
-            delete queryString.limit
-            const filters: IOfferFilter = {
-                ...queryString
-            }
-            const result = await this._useCase.execute({ filters, limit });
-            res.json(result);
-        } catch (error) {
-            if (error instanceof InvalidRequestError)
-                res.status(400)).send({ error: error.message, reason: error });
-            else res.status(500).send({ error: "Internal Server Error", reason: error });
-        }
+  public async handle(req: Request, res: Response) {
+    try {
+      let limit = parseInt(req.query.limit as string);
+      if (!limit) {
+        limit = 50;
+      }
+      const queryString = req.query;
+      delete queryString.limit;
+      const filters: IOfferFilter = {
+        ...queryString,
+      };
+      const result = await this._useCase.execute({ filters, limit });
+      res.json(result);
+    } catch (error) {
+      if (error instanceof InvalidRequestError)
+        res.status(400).send({ error: error.message, reason: error });
+      else
+        res.status(500).send({ error: "Internal Server Error", reason: error });
     }
+  }
 }
