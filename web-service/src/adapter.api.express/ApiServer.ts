47f4149bbdda1f2b6e8bfd1ad428db1ef0bfa934
LeBenjos <benjamin.schinkel01@outlook.fr>
2024-06-19 15:06:53 +0200

    merge: hotfix

--- web-service/src/adapter.api.express/ApiServer.ts
+++ web-service/src/adapter.api.express/ApiServer.ts
@@ -1,27 +1,26 @@
 import cors from "cors";
 import express, { Express, Router } from "express";
-import { auth } from "express-oauth2-jwt-bearer";
 import morgan from "morgan";
 
 export class ApiServer {
     private app: Express;
-    private corsOptions:cors.CorsOptions;
+    private corsOptions: cors.CorsOptions;
 
     constructor() {
-        this.corsOptions ={
-            origin:'*', 
-            credentials:true
-         }
+        this.corsOptions = {
+            origin: '*',
+            credentials: true
+        }
         this.app = express();
         this.app.use(cors(this.corsOptions));
         this.app.use(morgan("dev"));
-        this.app.use(
-            auth({
-                audience: "api.or.aus.floless.fr",
-                issuerBaseURL: "https://or-aus.eu.auth0.com/",
-                tokenSigningAlg: "RS256",
-            })
-        );
+        // this.app.use(
+        //     auth({
+        //         audience: "api.or.aus.floless.fr",
+        //         issuerBaseURL: "https://or-aus.eu.auth0.com/",
+        //         tokenSigningAlg: "RS256",
+        //     })
+        // );
     }
 
     public addRoute(router: Router) {
