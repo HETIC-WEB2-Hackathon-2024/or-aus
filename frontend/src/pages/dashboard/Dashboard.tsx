a0d610e61274909d9980e52a5a9f06f213c771c1
mirandalucas52 <mirandalucas52@gmail.com>
2024-06-19 14:27:34 +0200

    dashboard view

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,4 @@
-import Link from "next/link";
-import {
-    Activity,
-    ArrowUpRight,
-    CircleUser,
-    CreditCard,
-    DollarSign,
-    Menu,
-    Package2,
-    Search,
-    Users,
-} from "lucide-react";
+import { Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -19,202 +8,73 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 export function Dashboard() {
     return (
         <div className="flex min-h-screen w-full flex-col">
-            <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-                <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-                    <Link
-                        href="#"
-                        className="flex items-center gap-2 text-lg font-semibold md:text-base"
-                    >
-                        <Package2 className="h-6 w-6" />
-                        <span className="sr-only">Acme Inc</span>
-                    </Link>
-                    <Link
-                        href="#"
-                        className="text-foreground transition-colors hover:text-foreground"
-                    >
-                        Dashboard
-                    </Link>
-                    <Link
-                        href="#"
-                        className="text-muted-foreground transition-colors hover:text-foreground"
-                    >
-                        Orders
-                    </Link>
-                    <Link
-                        href="#"
-                        className="text-muted-foreground transition-colors hover:text-foreground"
-                    >
-                        Products
-                    </Link>
-                    <Link
-                        href="#"
-                        className="text-muted-foreground transition-colors hover:text-foreground"
-                    >
-                        Customers
-                    </Link>
-                    <Link
-                        href="#"
-                        className="text-muted-foreground transition-colors hover:text-foreground"
-                    >
-                        Analytics
-                    </Link>
-                </nav>
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button
-                            variant="outline"
-                            size="icon"
-                            className="shrink-0 md:hidden"
-                        >
-                            <Menu className="h-5 w-5" />
-                            <span className="sr-only">
-                                Toggle navigation menu
-                            </span>
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent side="left">
-                        <nav className="grid gap-6 text-lg font-medium">
-                            <Link
-                                href="#"
-                                className="flex items-center gap-2 text-lg font-semibold"
-                            >
-                                <Package2 className="h-6 w-6" />
-                                <span className="sr-only">Acme Inc</span>
-                            </Link>
-                            <Link href="#" className="hover:text-foreground">
-                                Dashboard
-                            </Link>
-                            <Link
-                                href="#"
-                                className="text-muted-foreground hover:text-foreground"
-                            >
-                                Orders
-                            </Link>
-                            <Link
-                                href="#"
-                                className="text-muted-foreground hover:text-foreground"
-                            >
-                                Products
-                            </Link>
-                            <Link
-                                href="#"
-                                className="text-muted-foreground hover:text-foreground"
-                            >
-                                Customers
-                            </Link>
-                            <Link
-                                href="#"
-                                className="text-muted-foreground hover:text-foreground"
-                            >
-                                Analytics
-                            </Link>
-                        </nav>
-                    </SheetContent>
-                </Sheet>
-                <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                    <form className="ml-auto flex-1 sm:flex-initial">
-                        <div className="relative">
-                            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                            <Input
-                                type="search"
-                                placeholder="Search products..."
-                                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-                            />
-                        </div>
-                    </form>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button
-                                variant="secondary"
-                                size="icon"
-                                className="rounded-full"
-                            >
-                                <CircleUser className="h-5 w-5" />
-                                <span className="sr-only">
-                                    Toggle user menu
-                                </span>
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Settings</DropdownMenuItem>
-                            <DropdownMenuItem>Support</DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>Logout</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-            </header>
             <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+                <h1 className="text-4xl font-bold py-6">
+                    Bon retour parmi nous
+                    <span className="text-primary"> Florent !</span> 👋
+                </h1>
                 <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
                     <Card x-chunk="dashboard-01-chunk-0">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                                Total Revenue
+                            <CardTitle className="text-xl font-b mb-2">
+                                Nouvelles offres dans votre commune
                             </CardTitle>
-                            <DollarSign className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">$45,231.89</div>
-                            <p className="text-xs text-muted-foreground">
-                                +20.1% from last month
+                        <CardContent className="ml-4">
+                            <div className="text-4xl font-bold text-primary">
+                                24
+                            </div>
+                            <p className="text-md text-[#71717A] mt-2">
+                                +20% par rapport au mois dernier
                             </p>
                         </CardContent>
                     </Card>
                     <Card x-chunk="dashboard-01-chunk-1">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                                Subscriptions
+                            <CardTitle className="text-xl font-semibold mb-2">
+                                Nouvelles offres dans votre secteur
                             </CardTitle>
-                            <Users className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">+2350</div>
-                            <p className="text-xs text-muted-foreground">
-                                +180.1% from last month
+                        <CardContent className="ml-4">
+                            <div className="text-4xl font-bold text-primary">
+                                349
+                            </div>
+                            <p className="text-lg text-[#71717A] mt-2">
+                                -11.5% par rapport au mois dernier
                             </p>
                         </CardContent>
                     </Card>
                     <Card x-chunk="dashboard-01-chunk-2">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                                Sales
+                            <CardTitle className="text-xl font-semibold mb-2">
+                                Nouvelles offres enregistrées
                             </CardTitle>
-                            <CreditCard className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">+12,234</div>
-                            <p className="text-xs text-muted-foreground">
-                                +19% from last month
+                        <CardContent className="ml-4">
+                            <div className="text-4xl font-bold text-primary">
+                                4
+                            </div>
+                            <p className="text-lg text-[#71717A] mt-2">
+                                -15% par rapport au mois dernier
                             </p>
                         </CardContent>
                     </Card>
                     <Card x-chunk="dashboard-01-chunk-3">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                            <CardTitle className="text-sm font-medium">
-                                Active Now
+                            <CardTitle className="text-xl font-semibold mb-2">
+                                Candidatures envoyées
                             </CardTitle>
-                            <Activity className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
-                        <CardContent>
-                            <div className="text-2xl font-bold">+573</div>
-                            <p className="text-xs text-muted-foreground">
-                                +201 since last hour
+                        <CardContent className="ml-4">
+                            <div className="text-4xl font-bold text-primary">
+                                2
+                            </div>
+                            <p className="text-lg text-[#71717A] mt-2">
+                                -10% par rapport au mois dernier
                             </p>
                         </CardContent>
                     </Card>
@@ -232,10 +92,7 @@ export function Dashboard() {
                                 </CardDescription>
                             </div>
                             <Button asChild size="sm" className="ml-auto gap-1">
-                                <Link href="#">
-                                    View All
-                                    <ArrowUpRight className="h-4 w-4" />
-                                </Link>
+                                <Link href="#">View All</Link>
                             </Button>
                         </CardHeader>
                     </Card>
@@ -249,7 +106,7 @@ export function Dashboard() {
                                     <p className="text-sm font-medium leading-none">
                                         Olivia Martin
                                     </p>
-                                    <p className="text-sm text-muted-foreground">
+                                    <p className="text-lg text-[#71717A] mt-2">
                                         olivia.martin@email.com
                                     </p>
                                 </div>
@@ -262,7 +119,7 @@ export function Dashboard() {
                                     <p className="text-sm font-medium leading-none">
                                         Jackson Lee
                                     </p>
-                                    <p className="text-sm text-muted-foreground">
+                                    <p className="text-lg text-[#71717A] mt-2">
                                         jackson.lee@email.com
                                     </p>
                                 </div>
@@ -275,7 +132,7 @@ export function Dashboard() {
                                     <p className="text-sm font-medium leading-none">
                                         Isabella Nguyen
                                     </p>
-                                    <p className="text-sm text-muted-foreground">
+                                    <p className="text-lg text-[#71717A] mt-2">
                                         isabella.nguyen@email.com
                                     </p>
                                 </div>
@@ -288,7 +145,7 @@ export function Dashboard() {
                                     <p className="text-sm font-medium leading-none">
                                         William Kim
                                     </p>
-                                    <p className="text-sm text-muted-foreground">
+                                    <p className="text-lg text-[#71717A] mt-2">
                                         will@email.com
                                     </p>
                                 </div>
@@ -301,7 +158,7 @@ export function Dashboard() {
                                     <p className="text-sm font-medium leading-none">
                                         Sofia Davis
                                     </p>
-                                    <p className="text-sm text-muted-foreground">
+                                    <p className="text-lg text-[#71717A] mt-2">
                                         sofia.davis@email.com
                                     </p>
                                 </div>
