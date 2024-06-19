970d0d942bf86c91f936de52a3a8950701938d9f
Louisan Tchitoula <t.louisan@outlook.fr>
2024-06-19 21:40:20 +0200

    Add logout logic

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -11,12 +11,15 @@ import {
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export type TCurrentView = "dashboard" | "parametres" | "offres" | "selection";
 interface HeaderProps {
     currentView: TCurrentView;
 }
 export default function Header({ currentView }: HeaderProps) {
+    const { logout } = useAuth0();
+
     return (
         <div className="flex w-full flex-col">
             <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-background px-4 md:px-6">
@@ -25,18 +28,23 @@ export default function Header({ currentView }: HeaderProps) {
                         <DropdownMenuTrigger asChild>
                             <div className="flex items-center border px-3 py-2 rounded-lg gap-4 cursor-pointer w-16 md:w-80">
                                 <CircleUser className="h-6 w-6" />
-                                <div className="hidden md:block">
-                                    Florent Fauchille
-                                </div>
+                                <div className="hidden md:block">Florent Fauchille</div>
                             </div>
                         </DropdownMenuTrigger>
-                        <DropdownMenuContent
-                            align="start"
-                            className="w-16 md:w-80"
-                        >
+                        <DropdownMenuContent align="start" className="w-16 md:w-80">
                             <DropdownMenuLabel>Mon compte</DropdownMenuLabel>
                             <DropdownMenuSeparator />
-                            <DropdownMenuItem>Logout</DropdownMenuItem>
+                            <DropdownMenuItem
+                                onClick={() =>
+                                    logout({
+                                        logoutParams: {
+                                            returnTo: window.location.origin,
+                                        },
+                                    })
+                                }
+                            >
+                                Logout
+                            </DropdownMenuItem>
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </div>
@@ -45,9 +53,7 @@ export default function Header({ currentView }: HeaderProps) {
                         <Link
                             to="dashboard"
                             className={`${
-                                currentView === "dashboard"
-                                    ? "text-primary"
-                                    : "text-secondary"
+                                currentView === "dashboard" ? "text-primary" : "text-secondary"
                             }  hover:text-primary `}
                         >
                             Dashboard
@@ -55,9 +61,7 @@ export default function Header({ currentView }: HeaderProps) {
                         <Link
                             to="offres"
                             className={`${
-                                currentView === "offres"
-                                    ? "text-primary"
-                                    : "text-secondary"
+                                currentView === "offres" ? "text-primary" : "text-secondary"
                             }  hover:text-primary `}
                         >
                             Offres
@@ -65,9 +69,7 @@ export default function Header({ currentView }: HeaderProps) {
                         <Link
                             to="parametres"
                             className={`${
-                                currentView === "parametres"
-                                    ? "text-primary"
-                                    : "text-secondary"
+                                currentView === "parametres" ? "text-primary" : "text-secondary"
                             }  hover:text-primary `}
                         >
                             Paramètres
@@ -75,9 +77,7 @@ export default function Header({ currentView }: HeaderProps) {
                         <Link
                             to="selection"
                             className={`${
-                                currentView === "selection"
-                                    ? "text-primary"
-                                    : "text-secondary"
+                                currentView === "selection" ? "text-primary" : "text-secondary"
                             }  hover:text-primary `}
                         >
                             Sélection
@@ -86,76 +86,44 @@ export default function Header({ currentView }: HeaderProps) {
                 </nav>
                 <Sheet>
                     <SheetTrigger asChild>
-                        <Button
-                            variant="outline"
-                            size="icon"
-                            className="shrink-0 md:hidden"
-                        >
+                        <Button variant="outline" size="icon" className="shrink-0 md:hidden">
                             <Menu className="h-5 w-5" />
-                            <span className="sr-only">
-                                Toggle navigation menu
-                            </span>
+                            <span className="sr-only">Toggle navigation menu</span>
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="left">
                         <nav className="grid gap-6 text-lg font-medium">
-                            <Link
-                                to="/dashboard"
-                                className="flex items-center gap-2 text-lg font-semibold"
-                            >
-                                <img
-                                    className="w-64"
-                                    src="/assets/logo.png"
-                                    alt=""
-                                />
+                            <Link to="/dashboard" className="flex items-center gap-2 text-lg font-semibold">
+                                <img className="w-64" src="/assets/logo.png" alt="" />
                             </Link>
                             <Link
                                 to="dashboard"
-                                className={`${
-                                    currentView === "dashboard"
-                                        ? "text-primary"
-                                        : "text-secondary"
-                                } `}
+                                className={`${currentView === "dashboard" ? "text-primary" : "text-secondary"} `}
                             >
                                 Dashboard
                             </Link>
                             <Link
                                 to="offres"
-                                className={`${
-                                    currentView === "offres"
-                                        ? "text-primary"
-                                        : "text-secondary"
-                                } `}
+                                className={`${currentView === "offres" ? "text-primary" : "text-secondary"} `}
                             >
                                 Offres
                             </Link>
                             <Link
                                 to="parametres"
-                                className={`${
-                                    currentView === "parametres"
-                                        ? "text-primary"
-                                        : "text-secondary"
-                                } `}
+                                className={`${currentView === "parametres" ? "text-primary" : "text-secondary"} `}
                             >
                                 Paramètres
                             </Link>
                             <Link
                                 to="selection"
-                                className={`${
-                                    currentView === "selection"
-                                        ? "text-primary"
-                                        : "text-secondary"
-                                } `}
+                                className={`${currentView === "selection" ? "text-primary" : "text-secondary"} `}
                             >
                                 Ma sélection
                             </Link>
                         </nav>
                     </SheetContent>
                 </Sheet>
-                <Link
-                    to="/dashboard"
-                    className="hidden lg:block absolute top-5 right-5"
-                >
+                <Link to="/dashboard" className="hidden lg:block absolute top-5 right-5">
                     <img className="w-64" src="/assets/logo.png" alt="" />
                 </Link>
             </header>
