18209609ad674feddc290f7019d986bf890aef95
lsun <lsun.github@gmail.com>
2024-06-19 13:15:23 +0200

    chore: Refactor ApiServer class in ApiServer.ts

--- web-service/src/adapter.api.express/ApiServer.ts
+++ web-service/src/adapter.api.express/ApiServer.ts
@@ -4,27 +4,27 @@ import { auth } from "express-oauth2-jwt-bearer";
 import morgan from "morgan";
 
 export class ApiServer {
-    private app: Express;
+  private app: Express;
 
-    constructor() {
-        this.app = express();
-        this.app.use(cors());
-        this.app.use(morgan("dev"));
-        this.app.use(express.json());
-        // this.app.use(
-        //     auth({
-        //         audience: "api.or.aus.floless.fr",
-        //         issuerBaseURL: "https://or-aus.eu.auth0.com/",
-        //         tokenSigningAlg: "RS256",
-        //     })
-        // );
-    }
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    this.app.use(
+      auth({
+        audience: "api.or.aus.floless.fr",
+        issuerBaseURL: "https://or-aus.eu.auth0.com/",
+        tokenSigningAlg: "RS256",
+      })
+    );
+  }
 
-    public addRoute(router: Router) {
-        this.app.use(router);
-    }
+  public addRoute(router: Router) {
+    this.app.use(router);
+  }
 
-    public listen(port: number, fn?: () => void) {
-        this.app.listen(port, fn);
-    }
+  public listen(port: number, fn?: () => void) {
+    this.app.listen(port, fn);
+  }
 }
