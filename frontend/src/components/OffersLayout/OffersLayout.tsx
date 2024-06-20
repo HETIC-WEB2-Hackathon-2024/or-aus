03b50ac3766f05c60887a3ed1ac9939bfad9b14c
Maria Petersen <maria.petersen@ealico.com>
2024-06-20 16:29:53 +0200

    clean up

--- frontend/src/components/OffersLayout/OffersLayout.tsx
+++ frontend/src/components/OffersLayout/OffersLayout.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { OfferDetail } from "../OfferDetail/OfferDetail"
 import OffersList from "@/components/OffersList/OffersList"
 import { IFilters, IOffer } from "@/pages/offers/Offers";
-import { useMediaQuery } from 'react-responsive'
 
 interface OffersLayoutProps {
     children?: React.ReactNode;
@@ -34,10 +33,6 @@ const intialSelectedOffer = {
 export default function OffersLayout({ children, filters, uri, isSelection }: OffersLayoutProps) {
 
     const [selectedOffer, setSelectedOffer] = useState<IOffer>(intialSelectedOffer)
-    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
     return (
         <div className="flex h-full">
