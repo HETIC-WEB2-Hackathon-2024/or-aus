46476ea5d5078b19450e740265135f7cc41efde1
Maria Petersen <maria.petersen@ealico.com>
2024-06-20 19:37:45 +0200

    clean up

--- frontend/src/auth/helper.ts
+++ frontend/src/auth/helper.ts
@@ -32,7 +32,7 @@ export async function authenticatedPost<T>(token: string, path: string, body: T)
     const data = await response.json();
     return data;
 }
-export async function authenticatedDelete<T>(token: string, path: string) {
+export async function authenticatedDelete(token: string, path: string) {
     const url = await makeUrl(path);
     const headers = makeHeaders(token);
     const response = await fetch(url, {
